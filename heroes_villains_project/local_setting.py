9d33e42300206f7a34a638bd0d0c8ce4d6dfc8fe
andrew-s-cole <andrew.s.cole@gmail.com>
2022-04-15 17:03:02 -0400

    Created Virtual Environment + Updated Settings Files

---
+++ heroes_villains_project/local_setting.py
@@ -0,0 +1,12 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-x_hw4%cd4^rcfqqkmtbq2!azf9$%)))qc*st#z0dbvt4^dn&f9'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'heroes_villains_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Celtics0314!!'
+    }
+}
